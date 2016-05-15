0dd7e7aa775397bd239235c129420267a5ea7202
Jacky Tran <jtran071@users.noreply.github.com>
2016-05-14 17:52:11 -0700

    added on and off button

--- app/src/main/java/com/app/soundscribe/soundscribe/MainActivity.java
+++ app/src/main/java/com/app/soundscribe/soundscribe/MainActivity.java
@@ -6,6 +6,8 @@ import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import be.tarsos.dsp.AudioDispatcher;
@@ -18,12 +20,18 @@ import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class MainActivity extends AppCompatActivity {
 
+Button off;
+    Button on;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+        on = (Button) findViewById(R.id.on);
+        off = (Button) findViewById(R.id.off);
+
+        final AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
 
         PitchDetectionHandler pdh = new PitchDetectionHandler() {
             @Override
@@ -38,9 +46,24 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         };
-        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
-        dispatcher.addAudioProcessor(p);
-        new Thread(dispatcher,"Audio Dispatcher").start();
+        final AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+
+        on.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dispatcher.addAudioProcessor(p);
+                new Thread(dispatcher,"Audio Dispatcher").start();
+            }
+        });
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatcher.removeAudioProcessor(p);
+            }
+        });
+
+
 
     }
 
